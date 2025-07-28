4d3c2bb80d701d3b56d6ea81696c31a01bd5c81d
俊杰 冯 <Axvser@outlook.com>
2025-07-28 15:13:03 +0800

    测试使用Newtonsoft.Json对WorkflowTree实施序列化与反序列化

--- Test/WpfApp2/MainWindow.xaml.cs
+++ Test/WpfApp2/MainWindow.xaml.cs
@@ -1,11 +1,21 @@
-﻿using System.Windows;
+﻿using Newtonsoft.Json;
+using System.Windows;
 using WpfApp2.ViewModels;
 
 namespace WpfApp2
 {
     public partial class MainWindow : Window
     {
-        FactoryViewModel fc;
+        private readonly JsonSerializerSettings settings = new()
+        {
+            TypeNameHandling = TypeNameHandling.Auto, // 允许接口与抽象类
+            NullValueHandling = NullValueHandling.Include, // 包含空值
+            Formatting = Formatting.Indented, // 格式对齐
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore, // 忽略循环引用
+            PreserveReferencesHandling = PreserveReferencesHandling.Objects, // 保留对象引用
+        };
+
+        FactoryViewModel? fc;
         public MainWindow()
         {
             InitializeComponent();
@@ -15,8 +25,11 @@ namespace WpfApp2
             {
                 Nodes = [node1, node2]
             };
-            fc = tree;
-            container.DataContext = tree;
+            string json = JsonConvert.SerializeObject(tree, settings);
+            var result = JsonConvert.DeserializeObject<FactoryViewModel>(json, settings);
+            MessageBox.Show(json);
+            container.DataContext = result;
+            fc = result;
         }
 
         private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
