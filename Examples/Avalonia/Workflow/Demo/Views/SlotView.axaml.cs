185e321d6a09b838857a2bf571e8186d6b64e460
俊杰 冯 <Axvser@outlook.com>
2025-08-30 14:49:30 +0800

    优化Avalonia对无鼠标设备上拖拽连接线的发射代码

--- Examples/Avalonia/Workflow/Demo/Views/SlotView.axaml.cs
+++ Examples/Avalonia/Workflow/Demo/Views/SlotView.axaml.cs
@@ -1,26 +1,41 @@
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
+using Avalonia.VisualTree;
 using VeloxDev.Core.Interfaces.WorkflowSystem;
+using VeloxDev.Core.WorkflowSystem;
 
 namespace Demo.Views;
 
 public partial class SlotView : UserControl
 {
+    private Canvas? _parentCanvas;
+    
     public SlotView()
     {
         InitializeComponent();
+    }
+    
+    protected override void OnAttachedToVisualTree(VisualTreeAttachmentEventArgs e)
+    {
+        base.OnAttachedToVisualTree(e);
+        _parentCanvas = this.FindAncestorOfType<Canvas>().FindAncestorOfType<Canvas>();
     }
 
     // 当触点按下，将 输入/输入口 设定为 输出口
     private void OnPointerPressed(object? sender, PointerPressedEventArgs e)
     {
-        if (DataContext is not IWorkflowSlot context) return;
+        if (DataContext is not IWorkflowSlot context  || context.Parent?.Parent is not { } tree) return;
 
         // Avalonia 必须这么做，否则 Released 无法作用在其它 SlotView
         if (e.Pointer.Captured != null)
         {
             e.Pointer.Capture(null);
         }
+        
+        // 无鼠标设备需要在此处更新一次当前位置，否则连接线可能有一帧异常数据
+        var point = e.GetPosition(_parentCanvas);
+        tree.SetMouseCommand.Execute(new Anchor(point.X, point.Y, 0));
 
         context.ConnectingCommand.Execute(null);
 
