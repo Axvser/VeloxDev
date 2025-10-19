6313c105b9d0215893e0a07801f5f2bf41a406d3
俊杰 冯 <Axvser@outlook.com>
2025-10-20 02:20:56 +0800

    改进Workflow的ViewModel的生成特性

--- VeloxDev.Core/WorkflowSystem/WorkflowBuilder.cs
+++ VeloxDev.Core/WorkflowSystem/WorkflowBuilder.cs
@@ -6,21 +6,47 @@ namespace VeloxDev.Core.WorkflowSystem
     {
         public sealed class ViewModel
         {
+            /// <summary>
+            /// [ Generator ] Template Code For Workflow Tree Component
+            /// </summary>
+            /// <typeparam name="T"> The Type Of Helper </typeparam>
+            /// <param name="virtualLinkType"> The Type Of VirtualLink </param>
+            /// <param name="virtualSlotType"> The Type Of Slot In VirtualLink </param>
             [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-            public sealed class TreeAttribute<T> : Attribute
-                where T : IWorkflowTreeViewModelHelper;
+            public sealed class TreeAttribute<T>(Type? virtualLinkType = default, Type? virtualSlotType = default) : Attribute
+                where T : IWorkflowTreeViewModelHelper, new()
+            {
+                public Type? VirtualLinkType { get; } = virtualLinkType;
+                public Type? VirtualSlotType { get; } = virtualSlotType;
+            }
 
+            /// <summary>
+            /// [ Generator ] Template Code For Workflow Node Component
+            /// </summary>
+            /// <typeparam name="T"> The Type Of Helper </typeparam>
             [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
             public sealed class NodeAttribute<T> : Attribute
-                where T : IWorkflowNodeViewModelHelper;
+                where T : IWorkflowNodeViewModelHelper, new();
 
+            /// <summary>
+            /// [ Generator ] Template Code For Workflow Slot Component
+            /// </summary>
+            /// <typeparam name="T"> The Type Of Helper </typeparam>
             [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
             public sealed class SlotAttribute<T> : Attribute
-                where T : IWorkflowSlotViewModelHelper;
+                where T : IWorkflowSlotViewModelHelper, new();
 
+            /// <summary>
+            /// [ Generator ] Template Code For Workflow Link Component
+            /// </summary>
+            /// <typeparam name="T"> The Type Of Helper </typeparam>
+            /// <param name="slotType"> The Type Of Initial Slot </param>
             [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
-            public sealed class LinkAttribute<T> : Attribute
-                where T : IWorkflowLinkViewModelHelper;
+            public sealed class LinkAttribute<T>(Type? slotType = default) : Attribute
+                where T : IWorkflowLinkViewModelHelper, new()
+            {
+                public Type? SlotType { get; } = slotType;
+            }
         }
 
         public sealed class View
