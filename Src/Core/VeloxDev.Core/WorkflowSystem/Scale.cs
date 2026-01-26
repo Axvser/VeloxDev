1569cc196b3ca68c887aea853f274ce913931d30
i_fengjunjie <i_fengjunjie@cvte.com>
2026-01-26 16:24:25 +0800

    [ Feature ] Layout System for workflow has been added to lib
    [ How ]
    1. User the viewmodel whose type is <Layout> in your tree viewmodel
    2. Binding the <ActualSize> for canvas
    3. Binding the <ActualOffset> for canvas
    …… you can see all in Examples/Workflow/Avalonia

---
+++ Src/Core/VeloxDev.Core/WorkflowSystem/Scale.cs
@@ -0,0 +1,30 @@
+﻿using VeloxDev.Core.AOT;
+using VeloxDev.Core.MVVM;
+
+namespace VeloxDev.Core.WorkflowSystem
+{
+    [AOTReflection(Constructors: true, Methods: true, Properties: true, Fields: true)]
+    public sealed partial class Scale(double x = 0d, double y = 0d) : ICloneable
+    {
+        [VeloxProperty]
+        private double _x = x;
+        [VeloxProperty]
+        private double _y = y;
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Scale size)
+            {
+                return X == size.X && Y == size.Y;
+            }
+            return false;
+        }
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+        public override string ToString() => $"Scale({X},{Y})";
+        public object Clone() => new Scale(X, Y);
+        public static bool operator ==(Scale a, Scale b) => a.Equals(b);
+        public static bool operator !=(Scale a, Scale b) => !a.Equals(b);
+        public static Scale operator +(Scale a, Scale b) => new(a.X + b.X, a.Y + b.Y);
+        public static Scale operator -(Scale a, Scale b) => new(a.X - b.X, a.Y - b.Y);
+    }
+}
