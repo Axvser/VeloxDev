d3b55a88f450c998b516cd58ab5474c8fb21571d
俊杰 冯 <Axvser@outlook.com>
2026-07-27 02:38:49 +0800

    Plan to use Cloud Glyph for WIki build

--- Docs/VeloxDev.Docs/Assets/Docs/en/2_DeepDive/index.md
+++
@@ -1,5 +0,0 @@
-# Deep Dive
-
-Architecture, class diagrams, design rationale, and advanced topics.
-
-Choose a topic from the sidebar to dive in.
--- Docs/VeloxDev.Docs/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace VeloxDev.Docs.ViewModels;
-
-public partial class MainViewModel : ObservableObject
-{
-    [ObservableProperty]
-    private DocumentViewModel _document = new();
-}
