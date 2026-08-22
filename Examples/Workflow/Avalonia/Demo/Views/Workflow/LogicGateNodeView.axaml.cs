ebc572dafc8b7d3e3427fdecbb3aa941c9367ab3
俊杰 冯 <Axvser@outlook.com>
2026-08-23 01:47:15 +0800

    Update Avalonia Workflow Demo

---
+++ Examples/Workflow/Avalonia/Demo/Views/Workflow/LogicGateNodeView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Demo;
+
+public partial class LogicGateNodeView : UserControl
+{
+    public LogicGateNodeView()
+    {
+        InitializeComponent();
+    }
+}
